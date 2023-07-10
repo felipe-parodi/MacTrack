7d5dbb589d18fc2ce625b6e90e2c623544281075
Felipe Parodi <fparodi@pennmedicine.upenn.edu>
2023-07-10 10:35:03 -0400

    Update pseudolabel_viddir2coco_3x.py

--- scripts/pseudolabel_viddir2coco_3x.py
+++ scripts/pseudolabel_viddir2coco_3x.py
@@ -51,7 +51,7 @@ def main():
     bbox_thr = args.bbox_thr
     checkpoint_interval = args.checkpoint_interval
     device = args.device
-    keypoint_thr = 0.5
+    keypoint_thr = 0.35
     min_num_keypoints_desired = 11
 
     os.makedirs(out_dir, exist_ok=True)
@@ -77,7 +77,10 @@ def main():
     visualizer.set_dataset_meta(
         pose_estimator.dataset_meta, skeleton_style=pose_estimator.cfg.skeleton_style
     )
-    categories = [{"id": 1, "name": "monkey", "supercategory": "monkey"}]
+    categories = [{"id": 1, "name": "monkey", "supercategory": "monkey", 
+                   "keypoints": ["nose", "left_eye", "right_eye", "left_ear", "right_ear", "left_shoulder", "right_shoulder", "left_elbow", "right_elbow", "left_wrist", "right_wrist", "left_hip", "right_hip", "left_knee", "right_knee", "left_ankle", "right_ankle"],
+                   "skeleton": [[16,14], [14,12], [17,15], [15,13], [12,13], [6,12], [7,13], [6,7], [6,8], [7,9], [8,10], [9,11], [2,3], [1,2], [1,3], [2,4], [3,5], [4,6], [5,7]]
+                   }]
 
     img_anno_dict = {
         "categories": categories,
@@ -179,7 +182,7 @@ def main():
                 frame_id_uniq_counter += 1
                 kpts_flat = []
                 for pt, pt_score in zip(kpts, kpts_scores):
-                    if pt_score > keypoint_thr:
+                    if pt_score >= keypoint_thr:
                         visibility = 2  # visible
                         visible_keypoints += 1
                     else:
